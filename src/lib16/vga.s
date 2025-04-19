b398bd6b83f2c73b6eb5c1ec34b6ab555edbcfdf
romheat <romheat@gmail.com>
2025-04-19 16:52:23 +0200

    clean

--- src/lib16/vga.s
+++ src/lib16/vga.s
@@ -202,7 +202,7 @@ vga_read_state_no_vga:
   mov     ax, #$1200
   mov     bx, #$0010
   int     $10
-  cmp     bl, #$10  
+  cmp     bl, #$10
   jne     vga_read_state_have_vga_or_ega     ; EGA detected
 
 vga_read_state_done:
